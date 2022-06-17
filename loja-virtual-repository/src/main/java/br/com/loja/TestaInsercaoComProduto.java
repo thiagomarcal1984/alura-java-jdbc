dbcb570114a273b96a0f453d7b49bcc8a0974ff6
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-17 18:47:49 -0300

    DAO com inserção do produto.

--- loja-virtual-repository/src/main/java/br/com/loja/TestaInsercaoComProduto.java
+++ loja-virtual-repository/src/main/java/br/com/loja/TestaInsercaoComProduto.java
@@ -1,11 +1,9 @@
 package br.com.loja;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+import br.com.loja.dao.ProdutoDAO;
 import br.com.loja.modelo.Produto;
 
 public class TestaInsercaoComProduto {
@@ -13,21 +11,9 @@ public class TestaInsercaoComProduto {
 		Produto comoda = new Produto("Cômoda", "Cômoda vertical");
 		
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
-			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?);";
-			
-			try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				pstm.setString(1, comoda.getNome());
-				pstm.setString(2, comoda.getDescricao());
-				
-				pstm.execute();
-				
-				try (ResultSet rst = pstm.getGeneratedKeys()) {
-					while (rst.next()) {
-						comoda.setId(rst.getInt(1));
-					}
-				}
-			}
+			ProdutoDAO produtoDao = new ProdutoDAO(connection);
+			produtoDao.salvar(comoda);
+//			List lista = persistenciaProduto.listar();
 		}
-		System.out.println(comoda);
 	}
 }
