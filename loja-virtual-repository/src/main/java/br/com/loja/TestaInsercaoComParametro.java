ee978bede5d04bb42b839eabc08de74c2ce083f9
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-17 16:32:03 -0300

    Lidando com commit e rollback.

--- loja-virtual-repository/src/main/java/br/com/loja/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/main/java/br/com/loja/TestaInsercaoComParametro.java
@@ -9,21 +9,39 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 	
 	public static void main(String[] args) throws SQLException {
-		String nome = "Mouse'"; 
-			// Note que a aspa simples deveria quebrar o SQL.
-		String descricao = "Mouse sem fio); delete from Produto;"; 
-			// Note o SQL Injection para deletar o que está na tabela Produto.
-		
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
 		
-		PreparedStatement stm = connection.prepareStatement(
-				"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?);"
-				, Statement.RETURN_GENERATED_KEYS
-		);
-		
+		connection.setAutoCommit(false); // Evita o commit automático das transações.
+
+		try {
+			PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?);"
+					, Statement.RETURN_GENERATED_KEYS
+			);
+			
+			adicionarVariavel("SmartTV", "45 polegadas", stm);
+			adicionarVariavel("Rádio", "Rádio de bateria", stm);
+			
+			connection.commit();
+			
+			stm.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO.");
+			connection.rollback();
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm)
+			throws SQLException {
 		stm.setString(1, nome); // Primeiro parâmetro da PreparedStatement.
 		stm.setString(2, descricao); // Segundo parâmetro da PreparedStatement.
+		//*
+		if (nome.equals("Rádio")) {
+			throw new RuntimeException("Não foi possível adicionar o produto");
+		} //*/
 		
 		stm.execute();
 		
@@ -35,8 +53,6 @@ public class TestaInsercaoComParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O ID criado foi: " + id);
 		}
-		
-		connection.close();
 	}
 
 }
